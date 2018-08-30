a5ffbe78f2dcb24ed6d7e4a06c99c985e7cbd0e9
rcanai <canai3jp@gmail.com>
2018-08-30 20:44:52 +0900

    nuxt.configのeslint対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -163,7 +163,7 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
+    extend (config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
@@ -178,7 +178,7 @@ module.exports = {
     // ベンダープレフィックス自動付与設定
     postcss: [
       require('autoprefixer')({
-        browsers: ['IE 11', 'last 2 versions' ],
+        browsers: ['IE 11', 'last 2 versions'],
         grid: true,
         cascade: false
       })
