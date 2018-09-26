c98092afb6f296601884f44a827dbebd13562016
rcanai <canai3jp@gmail.com>
2018-09-26 21:24:40 +0900

    アイコンのサイズを変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -151,7 +151,22 @@ module.exports = {
     display: 'standalone',
     icons: [
       {
-        'src': '/icons/72.png',
+        'src': '/icons/64.png',
+        'sizes': '64x64',
+        'type': 'image/png'
+      },
+      {
+        'src': '/icons/128.png',
+        'sizes': '128x128',
+        'type': 'image/png'
+      },
+      {
+        'src': '/icons/256.png',
+        'sizes': '256x256',
+        'type': 'image/png'
+      },
+      {
+        'src': '/icons/512.png',
         'sizes': '512x512',
         'type': 'image/png'
       }
